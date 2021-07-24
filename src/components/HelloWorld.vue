ee2efdf8a1718a0a6fc6031c60b25cc8fb14fe11
Alexander Lechthaler <alexander@lechthaler.online>
2021-07-24 11:40:48 +0200

    stage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,136 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <!-- <v-form ref="form"> -->
+    <v-card elevation="6">
+      <v-card-title>Personal Earnigs and Investments</v-card-title>
+
+      <v-card-text>
+        <v-row justify="space-between">
+          <v-col cols="4">
+            <v-text-field
+              v-model="income"
+              label="Income"
+              :rules="rules"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="investments"
+              label="Investments"
+              :rules="rules"
+              clearable
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field v-model="expenses" label="Expenses"></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row justify="space-between">
+          <v-col cols="2">
+            <v-text-field
+              v-model="inv_exp_ratio_comp"
+              label="Invest Expense Ratio"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="10">
+            <v-slider
+              v-model="inv_exp_ratio"
+              :min="slider_min"
+              :max="slider_max"
+              label=""
+            >
+            </v-slider>
+          </v-col>
+        </v-row>
+
+        <v-row justify="space-between">
+          <v-col cols="2">
+            <v-text-field
+              v-model="interest_rate"
+              label="Interest Rate"
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="10">
+            <v-slider
+              v-model="interest_rate"
+              :min="slider_min"
+              :max="slider_max"
+              label=""
+            >
+            </v-slider>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <!-- </v-form> -->
+  </v-container>
 </template>
+
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+
+  data: function () {
+    return {
+      slider_min: 0,
+      slider_max: 100,
+
+      income: 2000,
+      investments: 200,
+
+      inv_exp_ratio: 10,
+      interest_rate: 8,
+
+      rules: [
+        (value) => !!value || "Required.",
+        (value) => !!parseInt(value) || "Enter Number.",
+      ],
+    };
+  },
+
+  computed: {
+    expenses: {
+      get: function () {
+        return this.income - this.investments;
+      },
+      set: function (val) {
+        this.investments = this.income - val;
+      },
+    },
+
+    inv_exp_ratio_comp: {
+      get: function () {
+        return this.inv_exp_ratio / 10;
+      },
+      set: function (val) {
+        this.inv_exp_ratio = val * 10;
+
+        console.log("value=", val)
+        this.investments = parseInt(
+          this.income * val / (1 + val)
+        );
+        console.log(this.investments);
+      },
+    },
+  },
+  methods: {},
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .container {
+    border: 1px solid green;
+  }
+  .row {
+    border: 1px solid red;
+  }
+  .col {
+    border: 1px solid blue;
+  }
 </style>
+-->
