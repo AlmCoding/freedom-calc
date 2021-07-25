2f4414d24dd872eda1d0af0ccc9daeab12360069
Alexander Lechthaler <alexander@lechthaler.online>
2021-07-25 13:55:49 +0200

    comp trajectory§

--- src/components/FinancialInputs.vue
+++ src/components/FinancialInputs.vue
@@ -57,6 +57,7 @@
           <v-col cols="2">
             <v-text-field
               v-model="interest_rate"
+              @change="compute_trajectory"
               label="Interest Rate"
             ></v-text-field>
           </v-col>
@@ -64,6 +65,7 @@
           <v-col cols="10">
             <v-slider
               v-model="interest_rate"
+              @change="compute_trajectory"
               :min="slider_min"
               :max="slider_max"
               label=""
@@ -90,6 +92,7 @@ export default {
       income: 2000,
       investments: 1000,
       expenses: 1000,
+
       inv_exp_ratio: 1,
       inv_exp_ratio_10: 10,
 
@@ -99,48 +102,29 @@ export default {
         (value) => !!value || "Required.",
         (value) => !!parseInt(value) || "Enter Number.",
       ],
+
+      trajectory: [],
     };
   },
 
-  computed: {
-    /*
-    expenses_comp: {
-      get: function () {
-        return this.income - this.investments;
-      },
-      set: function (val) {
-        this.investments = this.income - val;
-      },
-    },
-
-    inv_exp_ratio_comp: {
-      get: function () {
-        return this.inv_exp_ratio / 10;
-      },
-      set: function (val) {
-        this.inv_exp_ratio = val * 10;
-
-        //console.log("value=", val);
-        //this.investments = parseInt((this.income * val) / (1 + val));
-        //console.log(this.investments);
-      },
-    },
-    */
-  },
+  computed: {},
 
   methods: {
     update_inv_exp_ratio: function () {
-      this.inv_exp_ratio = parseFloat(this.investments / this.expenses).toFixed(2);
+      this.inv_exp_ratio = parseFloat(this.investments / this.expenses).toFixed(
+        2
+      );
       this.inv_exp_ratio_10 = this.inv_exp_ratio * 10;
+      this.compute_trajectory();
     },
 
     update_inv_exp: function () {
-      //console.log(this.income, this.investments, this.expenses, this.inv_exp_ratio);
       this.investments = parseInt(
         (this.income * parseFloat(this.inv_exp_ratio)) /
           (1 + parseFloat(this.inv_exp_ratio))
       );
       this.expenses = this.income - this.investments;
+      this.compute_trajectory();
     },
 
     income_changed: function () {
@@ -163,6 +147,21 @@ export default {
     inv_exp_ratio_10_changed: function () {
       this.inv_exp_ratio = this.inv_exp_ratio_10 / 10;
       this.update_inv_exp();
+    },
+
+    compute_trajectory: function () {
+      this.trajectory = [];
+
+      var annual_investments = this.investments * 12;
+      var interest = this.interest_rate / 100;
+      var net_worth = 0;
+
+      for (let year = 0; year < 50; year++) {
+        net_worth = net_worth * (1 + interest);
+        net_worth += annual_investments;
+        this.trajectory.push(net_worth);
+      }
+      console.log(this.trajectory);
     },
   },
 };
