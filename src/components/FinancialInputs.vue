bb83d6d232cd1f241414cb62cd90e67fde17a2ac
Alexander Lechthaler <alexander@lechthaler.online>
2021-10-20 22:41:51 +0200

    add fire plan + refactoring

--- src/components/FinancialInputs.vue
+++ src/components/FinancialInputs.vue
@@ -1,11 +1,12 @@
 <template>
   <v-container>
-    <v-card elevation="6">
-      <v-card-title>Personal Earnigs and Investments</v-card-title>
+
+    <v-card tile elevation="6">
+      <v-card-title>Current Monthly Numbers</v-card-title>
 
       <v-card-text>
-        <v-row justify="space-between">
-          <v-col cols="4">
+        <v-row dense justify="space-between">
+          <v-col cols="6">
             <v-text-field
               v-model="income"
               @change="income_changed"
@@ -14,7 +15,7 @@
               clearable
             ></v-text-field>
           </v-col>
-          <v-col cols="4">
+          <v-col cols="6">
             <v-text-field
               v-model="investments"
               @change="investments_changed"
@@ -23,6 +24,7 @@
               clearable
             ></v-text-field>
           </v-col>
+          <!--
           <v-col cols="4">
             <v-text-field
               v-model="expenses"
@@ -31,20 +33,21 @@
               clearable
             ></v-text-field>
           </v-col>
+          -->
         </v-row>
 
-        <v-row justify="space-between">
-          <v-col cols="2">
+        <v-row dense justify="space-between">
+          <v-col cols="4">
             <v-text-field
-              v-model="inv_exp_ratio"
-              @change="inv_exp_ratio_changed"
-              label="Invest Expense Ratio"
+              v-model="savings_rate"
+              @change="update_inv_exp"
+              label="Savings Rate"
             ></v-text-field>
           </v-col>
-          <v-col cols="10">
+          <v-col cols="8">
             <v-slider
-              v-model="inv_exp_ratio_10"
-              @change="inv_exp_ratio_10_changed"
+              v-model="savings_rate"
+              @change="update_inv_exp"
               :min="slider_min"
               :max="slider_max"
               label=""
@@ -53,29 +56,27 @@
           </v-col>
         </v-row>
 
-        <v-row justify="space-between">
-          <v-col cols="2">
+        <v-row dense justify="space-between">
+          <v-col cols="6">
             <v-text-field
-              v-model="interest_rate"
+              v-model="income_growth_rate"
               @change="compute_trajectory"
-              label="Interest Rate"
+              label="Income Growth Rate"
+              clearable
             ></v-text-field>
           </v-col>
-
-          <v-col cols="10">
-            <v-slider
-              v-model="interest_rate"
+          <v-col cols="6">
+            <v-text-field
+              v-model="max_annual_income"
               @change="compute_trajectory"
-              :min="slider_min"
-              :max="slider_max"
-              label=""
-            >
-            </v-slider>
+              label="Max. Annual Income"
+              clearable
+            ></v-text-field>
           </v-col>
         </v-row>
 
-        <v-row justify="space-between">
-          <v-col cols="12">
+        <v-row dense justify="space-between">
+          <v-col cols="6">
             <v-text-field
               v-model="current_networth"
               @change="compute_trajectory"
@@ -83,11 +84,81 @@
               clearable
             ></v-text-field>
           </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="current_age"
+              @change="compute_trajectory"
+              label="Current Age"
+              clearable
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row dense justify="space-between">
+          <v-col cols="4">
+            <v-text-field
+              v-model="interest_rate"
+              @change="compute_trajectory"
+              label="Interest Rate"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="8">
+            <v-slider
+              v-model="interest_rate"
+              @change="compute_trajectory"
+              :min="slider_min"
+              :max="slider_max"
+              label=""
+            >
+            </v-slider>
+          </v-col>
         </v-row>
       </v-card-text>
+    </v-card>
 
+    <v-spacer></v-spacer>
+
+    <v-card tile elevation="6">
+      <v-card-title>Coast FIRE Plan</v-card-title>
+      <!--
+      <v-row justify="space-between">
+        <v-col cols="6">
+          <v-card-title>Coast FIRE Plan</v-card-title>
+        </v-col>
+        <v-col cols="6">
+          <v-switch v-model="switch1" label="Coast FIRE"></v-switch>
+        </v-col> 
+      </v-row>-->
+
+      <v-card-text>
+        <v-row dense justify="space-between">
+          <v-col cols="6">
+            <v-text-field
+              v-model="coast_fire_expenses"
+              @change="compute_trajectory"
+              label="Monthly FIRE Expenses"
+              clearable
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="6">
+            <v-text-field
+              v-model="coast_fire_age"
+              @change="compute_trajectory"
+              label="Coast FIRE Age"
+              clearable
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+
+
+    <v-card tile elevation="6">
       <FinancialVisualization :trajectory="trajectory" />
     </v-card>
+
+
   </v-container>
 </template>
 
@@ -110,12 +181,16 @@ export default {
       income: 5000,
       investments: 3000,
       expenses: 2000,
+      current_age: 27,
+      coast_fire_age: 44,
+      coast_fire_expenses: 2000,
 
-      inv_exp_ratio: 1.5,
-      inv_exp_ratio_10: 15,
+      savings_rate: 60,
+      income_growth_rate: 1,
+      max_annual_income: 80000,
 
-      interest_rate: 5,
-      current_networth: 100000,
+      interest_rate: 7,
+      current_networth: 125000,
 
       rules: [
         (value) => !!value || "Required.",
@@ -123,6 +198,7 @@ export default {
       ],
 
       trajectory: [],
+      year_of_freedom: 30,
     };
   },
 
@@ -133,57 +209,61 @@ export default {
   computed: {},
 
   methods: {
-    update_inv_exp_ratio: function () {
-      this.inv_exp_ratio = parseFloat(this.investments / this.expenses).toFixed(
-        2
-      );
-      this.inv_exp_ratio_10 = this.inv_exp_ratio * 10;
+    update_savings_rate: function () {
+      this.savings_rate = parseInt((100 * this.expenses) / this.income);
       this.compute_trajectory();
     },
 
     update_inv_exp: function () {
-      this.investments = parseInt(
-        (this.income * parseFloat(this.inv_exp_ratio)) /
-          (1 + parseFloat(this.inv_exp_ratio))
-      );
+      this.investments = (this.income * this.savings_rate) / 100;
       this.expenses = this.income - this.investments;
       this.compute_trajectory();
     },
 
     income_changed: function () {
       this.investments = this.income - this.expenses;
-      this.update_inv_exp_ratio();
+      this.update_inv_exp();
     },
     investments_changed: function () {
       this.expenses = this.income - this.investments;
-      this.update_inv_exp_ratio();
+      this.update_savings_rate();
     },
     expenses_changed: function () {
       this.investments = this.income - this.expenses;
-      this.update_inv_exp_ratio();
-    },
-    inv_exp_ratio_changed: function () {
-      this.inv_exp_ratio_10 = this.inv_exp_ratio * 10;
-      this.update_inv_exp();
-    },
-    inv_exp_ratio_10_changed: function () {
-      this.inv_exp_ratio = this.inv_exp_ratio_10 / 10;
-      this.update_inv_exp();
+      this.update_savings_rate();
     },
 
     compute_trajectory: function () {
       this.trajectory = [];
 
       var annual_investments = this.investments * 12;
+      var max_annual_investments =
+        (this.max_annual_income * this.savings_rate) / 100;
       var interest = this.interest_rate / 100;
       var networth = this.current_networth;
 
+      this.year_of_freedom = 30;
+
       for (let year = 0; year < 30; year++) {
-        networth = networth * (1 + interest);
+        networth *= 1 + interest;
         networth += annual_investments;
         this.trajectory.push(parseInt(networth));
+
+        if (
+          networth * interest > this.expenses * 12 &&
+          year < this.year_of_freedom
+        ) {
+          this.year_of_freedom = year;
+        }
+
+        //console.log(annual_investments);
+        annual_investments *= 1 + this.income_growth_rate / 100;
+        if (annual_investments > max_annual_investments) {
+          annual_investments = max_annual_investments;
+        }
       }
       console.log(this.trajectory);
+      console.log(this.year_of_freedom);
     },
   },
 };
